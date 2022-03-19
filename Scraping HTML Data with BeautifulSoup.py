e8668726592f5645357a28597e80cc0067fbe483
James Kayik Choi <62376291+SirPhysics@users.noreply.github.com>
2022-03-20 03:33:02 +1100

    Create Scraping HTML Data with BeautifulSoup.py

---
+++ Scraping HTML Data with BeautifulSoup.py
@@ -0,0 +1,24 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+count = 0 
+sum = 0
+# Retrieve all of the anchor tags
+tags = soup('span')
+for tag in tags:
+    # Look at the parts of a tag
+    # count the number of tags 
+    # sum up the digit of tag.contents[0]
+    count += 1
+    sum = sum + int(tag.contents[0])
+print(sum)
+print(count)
